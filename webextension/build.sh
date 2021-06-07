9dc7ca7b685c1472ba776366e85902b24cb8907c
Devilish Spirits <devilishspirits@gmail.com>
2021-06-07 21:49:55 +0200

    [webextension] Fixed webextension ZIP build and switched .xpi -> .zip extension

--- webextension/build.sh
+++ webextension/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # Usage : cd-directory zip-executable output inputs...
 OUTPUT="$PWD/$3"
-cd "$1"
+CDDIR="$1/"
+cd "$CDDIR"
 PROG_ZIP="$2"
 shift 3
 rm -f "$OUTPUT" # Remove old ZIP
-exec $PROG_ZIP "$OUTPUT" $*
+exec $PROG_ZIP "$OUTPUT" $(for file in $*; do echo ${file:${#CDDIR}}; done)
