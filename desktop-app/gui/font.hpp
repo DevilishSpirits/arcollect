3c32dd7421cdccec6d3bdd535c7597ae89946994
Devilish Spirits <devilishspirits@gmail.com>
2021-06-10 01:24:01 +0200

    [desktop-app] Added `#pragma once` in 'gui/font.hpp'

--- desktop-app/gui/font.hpp
+++ desktop-app/gui/font.hpp
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+#pragma once
 #include "../sdl2-hpp/SDL.hpp"
 #include <SDL_ttf.h>
 #include <memory>
