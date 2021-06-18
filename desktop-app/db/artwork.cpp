8d4f5a854aa8d08e2a1e7e3d42735b6e2fcc6874
Devilish Spirits <devilishspirits@gmail.com>
2021-06-18 23:13:11 +0200

    [desktop-app] Arcollect::db::artwork::db_delete() now call Arcollect::local_data_version_changed()

--- desktop-app/db/artwork.cpp
+++ desktop-app/db/artwork.cpp
@@ -203,5 +203,7 @@ int Arcollect::db::artwork::db_delete(void)
 	// Erase on disk
 	std::filesystem::remove(Arcollect::path::artwork_pool / std::to_string(art_id));
 	std::cerr << "Artwork \"" << art_title << "\" (" << art_id << ") has been deleted" << std::endl;
+	// Update data_version
+	Arcollect::local_data_version_changed();
 	return 0;
 }
