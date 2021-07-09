df0119a1308250d0c741293de77cb2c9200a8c14
Devilish Spirits <devilishspirits@gmail.com>
2021-07-09 15:10:33 +0200

    [webextension/tests] Added e621 in-browser tests

---
+++ webextension/tests/platform_e621.py
@@ -0,0 +1,21 @@
+import time
+
+class Platform:
+	def __init__(self,webdriver):
+		self.webdriver = webdriver
+		# Click on the  to use cookies 🍪️
+		print('# Agree that the user is over 18+')
+		self.webdriver.navigateTo('https://www.e621.net/posts') # TODO Check success
+		self.webdriver.GetElement('css selector','button#guest-warning-accept').Click()
+	def __call__(self):
+		# Get the 'Save in Arcollect' button
+		Save_in_Arcollect = self.webdriver.GetElement('link text','Save in Arcollect')
+		Save_in_Arcollect.Click()
+		
+		while Save_in_Arcollect.GetText() == 'Save in Arcollect':
+			time.sleep(1)
+		
+		while Save_in_Arcollect.GetText() == 'Saving...':
+			time.sleep(1)
+		
+		return Save_in_Arcollect.GetText() == 'Saved'
