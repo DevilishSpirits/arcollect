614de281a5545f5faf8f00b588968f616313a9a6
Devilish Spirits <devilishspirits@gmail.com>
2021-07-09 13:45:24 +0200

    [webext-adder/tests] Added test for `tags` and `tag_acc_links` tables

--- webext-adder/tests/db_check.py
+++ webext-adder/tests/db_check.py
@@ -40,9 +40,9 @@ def test_count(test_set):
 	#result  = len(db_line_count_checks)      # DB line count coherency checks
 	result  = len(test_set['artworks'])      # DB artworks table check
 	result += len(test_set['accounts'])      # DB accounts table check
-	#result += len(test_set['tags'])          # DB tags table check
+	result += len(test_set['tags'])          # DB tags table check
 	result += len(test_set['art_acc_links']) # DB art_acc_links table check
-	#result += len(test_set['art_tag_links']) # DB art_tag_links table check
+	result += len(test_set['art_tag_links']) # DB art_tag_links table check
 	return result
 
 def check_db(test_num, db, test_set):
@@ -71,7 +71,7 @@ def check_db(test_num, db, test_set):
 		if len(mismatchs) == 0:
 			print('ok',test_num,'- Checking artwork',artwork['source'])
 		else:
-			print('not ok',test_num,'- Checking artwork',artwork['source'],'# ',', '.join(mismatchs))
+			print('not ok',test_num,'- Checking artwork',artwork['source'],'#',', '.join(mismatchs))
 	
 	# Check accounts
 	print('# Checking "accounts" table')
@@ -95,7 +95,29 @@ def check_db(test_num, db, test_set):
 		if len(mismatchs) == 0:
 			print('ok',test_num,'- Checking account',account['id'])
 		else:
-			print('not ok',test_num,'- Checking account',account['id'],'# ',', '.join(mismatchs))
+			print('not ok',test_num,'- Checking account',account['id'],'#',', '.join(mismatchs))
+	
+	# Check tags
+	print('# Checking "tags" table')
+	for account in test_set['tags']:
+		test_num += 1
+		account_db = db.execute('SELECT tag_title, tag_kind FROM tags WHERE tag_platid = ? AND tag_platform = ?;',(account['id'],platform)).fetchone()
+		
+		expected_values = (
+			('tag_title'   ,account.setdefault('title',None)),
+			('tag_kind'    ,account.setdefault('kind',None)),
+		)
+		mismatchs = []
+		for i in range(len(expected_values)):
+			expected = expected_values[i][1]
+			got      = account_db[i]
+			if expected != got:
+				mismatchs.append(expected_values[i][0]+' mismatch (expected:'+str(expected)+', got:'+str(got)+')')
+		
+		if len(mismatchs) == 0:
+			print('ok',test_num,'- Checking account',account['id'])
+		else:
+			print('not ok',test_num,'- Checking account',account['id'],'#',', '.join(mismatchs))
 	
 	# Check art_acc_links
 	print('# Checking "art_acc_links" table')
@@ -109,4 +131,17 @@ def check_db(test_num, db, test_set):
 			ok_string = 'not ok'
 		
 		print(ok_string,test_num,'- Checking',art_acc_link['artwork'],'to',art_acc_link['account'],art_acc_link['link'],'link # Found',count_in_db,'time(s)')
+	
+	# Check art_tag_links
+	print('# Checking "art_tag_links" table')
+	for art_tag_link in test_set['art_tag_links']:
+		test_num += 1
+		count_in_db = db.execute('SELECT COUNT(*) FROM art_tag_links NATURAL JOIN artworks NATURAL JOIN tags WHERE art_source = ? AND tag_platid = ?;',[art_tag_link['artwork'],art_tag_link['tag']]).fetchone()[0]
+		
+		if count_in_db == 1:
+			ok_string = 'ok'
+		else:
+			ok_string = 'not ok'
+		
+		print(ok_string,test_num,'- Checking',art_tag_link['tag'],'tag on',art_tag_link['artwork'],'# Found',count_in_db,'time(s)')
 	return test_num
