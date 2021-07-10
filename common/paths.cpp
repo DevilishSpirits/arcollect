954819dac4186d123ef25e88ed31b5bc15cce1b1
Devilish Spirits <devilishspirits@gmail.com>
2021-07-10 12:27:26 +0200

    [common] On Windows, save in LocalAppData folder

--- common/paths.cpp
+++ common/paths.cpp
@@ -1,10 +1,26 @@
 #include <arcollect-paths.hpp>
 #include <cstdlib>
+#if defined(_WIN32)
+#include <windows.h>
+#include <shlobj.h>
+#endif
 const std::filesystem::path Arcollect::path::xdg_config_home = [](){
 	// Compute the data home
 	if (auto* config_home = std::getenv("XDG_CONFIG_HOME"); config_home && *config_home != '\0')
 		return std::filesystem::path(config_home);
-	else return std::filesystem::path(std::getenv("HOME")) / ".config";
+	else {
+		#if defined(_WIN32)
+		// Store in Local AppData folder
+		PWSTR ppszPath;
+		SHGetKnownFolderPath(FOLDERID_LocalAppData,KF_FLAG_CREATE,NULL,&ppszPath); // TODO Check result
+		std::filesystem::path result(ppszPath);
+		CoTaskMemFree(ppszPath);
+		result /= "Arcollect";
+		return result;
+		#else // WITH_XDG
+		return std::filesystem::path(std::getenv("HOME")) / ".config";
+		#endif
+	}
 }();
 
 const std::filesystem::path Arcollect::path::arco_data_home = [](){
@@ -12,8 +28,15 @@ const std::filesystem::path Arcollect::path::arco_data_home = [](){
 	std::filesystem::path data_home;
 	if (auto* xdg_home = std::getenv("XDG_DATA_HOME"); xdg_home && *xdg_home != '\0')
 		data_home = std::filesystem::path(xdg_home) / "arcollect";
-	else
+	else {
+		#if defined(_WIN32)
+		// Store in local AppData folder (same dir as config)
+		// FIXME FOLDERID_SavedPicturesLibrary ?
+		data_home = Arcollect::path::xdg_config_home;
+		#else // WITH_XDG
 		data_home = std::filesystem::path(std::getenv("HOME")) / ".local" / "share" / "arcollect";
+		#endif
+	}
 	// Create the directory if it does not exist
 	std::filesystem::create_directory(data_home);
 	
