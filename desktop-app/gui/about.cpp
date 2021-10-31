a0cbbda95cf4786910fdc9b1e398aa366735ebb7
Devilish Spirits <devilishspirits@gmail.com>
2021-10-31 15:29:42 +0100

    [desktop-app] No longer hardcode "Your personal visual artwork library." (ARCOLLECT_SUMMARY)

--- desktop-app/gui/about.cpp
+++ desktop-app/gui/about.cpp
@@ -90,7 +90,7 @@ void Arcollect::gui::about_window::render(SDL::Rect target)
 		Elements elements;
 		elements << Align::CENTER
 			<< FontSize(48) << U"Arcollect "sv ARCOLLECT_VERSION_STR "\n\n"
-			<< FontSize(22) << U"Your personal visual artwork library\n"sv
+			<< FontSize(22) << U""sv ARCOLLECT_SUMMARY_STR "\n"
 			<< FontSize(16) << U""sv ARCOLLECT_WEBSITE_STR "\n"
 			<< U"Powered by:\n"sv
 			   CPP_STD_STR " ⸱ " CXX_COMPILER_TITLE_STR " ⸱ SDL2 ⸱ SQLite3 ⸱ OpenImageIO ⸱ LittleCMS ⸱ FreeType2 ⸱ HarfBuzz ⸱ Many other…\n"
