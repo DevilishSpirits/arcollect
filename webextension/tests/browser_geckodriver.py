37358dee7d4f1e548a293eba64667e64475b789e
Devilish Spirits <devilishspirits@gmail.com>
2021-12-23 01:23:02 +0100

    [GeckoDriver tests] Now copy the NativeManifest in the right path

--- webextension/tests/browser_geckodriver.py
+++ webextension/tests/browser_geckodriver.py
@@ -7,10 +7,13 @@ import requests
 
 class Browser:
 	def test_num():
-		return 4
+		return 3
 	def __init__(self):
 		# Install manifest
-		print('not ok 1 - Install native-messaging manifest # TODO Install manifest logic')
+		appmanifest_dest = os.environ['HOME']+'/'+os.environ['FIREFOX_APPMANIFEST_DEST']
+		os.makedirs(os.path.dirname(appmanifest_dest),exist_ok=True)
+		os.unlink(appmanifest_dest)
+		os.link(os.environ['FIREFOX_APPMANIFEST_SOURCE'],appmanifest_dest)
 		# Spawn geckodriver
 		# TODO Do not close stdout
 		self.geckodriver = Popen(['geckodriver'],stdout=DEVNULL)
@@ -24,14 +27,14 @@ class Browser:
 				}
 			}
 		}
-		self.webdriver = WebDriver.WebDriver(2,'http://127.0.0.1:4444/',session_start_data)
+		self.webdriver = WebDriver.WebDriver(1,'http://127.0.0.1:4444/',session_start_data)
 		# Install addon
-		WebDriver.assert_request_or_bailout(self.webdriver.post('moz/addon/install', {'path': WebDriver.extension_path,'temporary': True}),3,'Install Arcollect addon','Failed to install Arcollect addon!')
+		WebDriver.assert_request_or_bailout(self.webdriver.post('moz/addon/install', {'path': WebDriver.extension_path,'temporary': True}),2,'Install Arcollect addon','Failed to install Arcollect addon!')
 		# Install uBlock Origin
 		if 'UBLOCK_ORIGIN_XPI_PATH' in os.environ:
-			WebDriver.assert_request_or_bailout(self.webdriver.post('moz/addon/install', {'path': os.environ['UBLOCK_ORIGIN_XPI_PATH']}),4,'Install uBlock Origin','Failed to install uBlock Origin addon!')
+			WebDriver.assert_request_or_bailout(self.webdriver.post('moz/addon/install', {'path': os.environ['UBLOCK_ORIGIN_XPI_PATH']}),3,'Install uBlock Origin','Failed to install uBlock Origin addon!')
 		else:
-			print('ok 4 - Install uBlock Origin # SKIP $UBLOCK_ORIGIN_XPI_PATH is not set')
+			print('ok 3 - Install uBlock Origin # SKIP $UBLOCK_ORIGIN_XPI_PATH is not set')
 	
 	def __del__(self):
 		self.webdriver = None # Force WebDriver destruction
