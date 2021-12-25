6ee34cd2c8f8c10a74e0c74dfd916959a8481213
Devilish Spirits <devilishspirits@gmail.com>
2021-12-25 16:38:43 +0100

    [webext-adder tests] Added https://github.com/nst/JSONTestSuite parsing tests
    	Okay my parser is shit. Let's work on it!

--- webext-adder/tests/test_wtf_json_parser.py
+++ webext-adder/tests/test_wtf_json_parser.py
@@ -8,15 +8,18 @@ test_num = 1
 for filename in sys.argv[2:]:
 	data = open(filename,'r', encoding="utf-8").read()
 	json_file = json.loads(data)
-	parsed_json = Popen(test_wtf_json_parser,stdin=PIPE,stdout=PIPE).communicate(input=data.encode('utf-8'))[0].decode('utf-8')
 	try:
+		parsed_json = Popen(test_wtf_json_parser,stdin=PIPE,stdout=PIPE).communicate(input=data.encode('utf-8'))[0].decode('utf-8')
 		json_parsed = json.loads(parsed_json)
-	except:
-		json_parsed = None
-	if json_file != json_parsed:
+	except Exception as e:
 		print('not', end = ' ')
+		print('\n# Exception while parsing',filename,'result:', e, file = sys.stderr)
 		print('\n# Parser sent this', parsed_json, file = sys.stderr)
-		print('\n# I understood that', json.dumps(json_parsed), file = sys.stderr)
-		print('\n# I expected that', json.dumps(json_file), file = sys.stderr)
+	else:
+		if json_file != json_parsed:
+			print('not', end = ' ')
+			print('\n# Parser sent this', parsed_json, file = sys.stderr)
+			print('\n# I understood that', json.dumps(json_parsed), file = sys.stderr)
+			print('\n# I expected that', json.dumps(json_file), file = sys.stderr)
 	print('ok',test_num,'-',filename)
 	test_num += 1
