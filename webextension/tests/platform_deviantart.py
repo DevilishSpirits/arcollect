5cdc5f3fc21a6d4452c3be1577879d2c5803160f
Devilish Spirits <devilishspirits@gmail.com>
2023-03-20 17:40:01 +0100

    [DeviantArt content-script tests] Automatically click on cookie banners
    	There is TWO banners to click. Now it's automatic.

--- webextension/tests/platform_deviantart.py
+++ webextension/tests/platform_deviantart.py
@@ -6,14 +6,29 @@ class Platform:
 		self.webdriver = webdriver
 	def __call__(self,artwork):
 		# Find the 'Save in Arcollect' button by hrfe
-		button_elements = self.webdriver.GetElements('css selector','button')
 		print("# Search 'Save in Arcollect' button")
 		Save_in_Arcollect = None
-		for button in button_elements:
-			if button.GetProperty('textContent') == 'Save in Arcollect':
-				print("# Found 'Save in Arcollect' button")
-				Save_in_Arcollect = button
-				break
+		while Save_in_Arcollect is None:
+			time.sleep(1)
+			for button in self.webdriver.GetElements('css selector','button'):
+				textContent = button.GetProperty('textContent').upper()
+				if textContent == 'SAVE IN ARCOLLECT':
+					print("# Found 'Save in Arcollect' button, clicking...")
+					if button.Click().ok:
+						Save_in_Arcollect = button
+						break
+					else:
+						print('# Click failed likely due to a cookie overlay or obstruction')
+				elif textContent.upper() == 'DISAGREE':
+					# Click on the no cookies button
+					print("# Found a 'Disagree' button, likely the cookie banner so click it")
+					button.Click()
+					break # Try again
+				elif textContent.upper() == 'ACCEPT':
+					# Click on the no cookies button
+					print("# Found an 'Accept' button, likely the second cookie banner so click it")
+					button.Click()
+					break # Try again
 		Save_in_Arcollect.Click()
 		
 		while Save_in_Arcollect.GetText() == 'Save in Arcollect':
